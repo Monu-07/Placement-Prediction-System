9efb54fa29a3704ff30d061196823071ca42d960
Monu-07 <2500030622@kluniversity.in>
2026-08-03 15:20:32 +0530

    Day-3 updates

---
+++ src/data/load_data.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+def load_data():
+    df=pd.read_csv("C:/Users/asolo/OneDrive/Documents/ML/Placement-Prediction-System/data/placement_data.csv")
+    return df
+def get_summary(df):
+    return {
+        "rows": df.shape[0],
+        "columns": df.shape[1],
+        "target": "PlacementStatus"
+    }
+if __name__ == "__main__":
+    df = load_data()
+    print(get_summary(df))
