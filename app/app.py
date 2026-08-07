ff60ed1f8197962c9eef2ac6fd185d60e5f480f2
Monu-07 <2500030622@kluniversity.in>
2026-08-07 14:01:10 +0530

    Day-4 updates

--- app/app.py
+++ app/app.py
@@ -1,26 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 from src.data.load_data import load_data, get_summary
+import os
 
 app = Flask(__name__)
-
 
 @app.route("/")
 def home():
     return render_template("home.html")
 
-
 @app.route("/dataset")
 def dataset():
-
+    # Load your dataset
     df = load_data()
     summary = get_summary(df)
 
+    # Pass summary + first rows to template
     return render_template(
         "load_dataset.html",
         summary=summary,
         first_rows=df.head().to_html(index=False)
     )
 
+@app.route("/eda")
+def eda():
+    return render_template("eda.html")
+
+# Serve images from results/images
+@app.route("/results/images/<path:filename>")
+def results_images(filename):
+    # Use absolute path to be safe
+    base_dir = os.path.join(os.path.dirname(__file__), "..", "results", "images")
+    return send_from_directory(base_dir, filename)
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ src/data/eda.py
@@ -0,0 +1,87 @@
+from statistics import correlation
+from src.data.load_data import load_data
+import matplotlib.pyplot as plt
+import os
+import pandas as pd
+import seaborn as sns
+
+BASE_DIR = os.path.join("app", "static", "images")
+os.makedirs(BASE_DIR, exist_ok=True)
+
+def basic_eda(df):
+    print("First Five Rows")
+    print(df.head())
+    print("Last Five Rows")
+    print(df.tail())
+    print("\n25 to 30 records:")
+    print(df.iloc[24:30])
+    print("Datatypes")
+    print(df.dtypes)
+    print("Complete Information")
+    print(df.info())
+    print("Duplicates")
+    print(df.duplicated())
+    print("Null Values")
+    print(df.isnull().sum())
+    print(df["PlacementStatus"].value_counts())
+    count = df["PlacementStatus"].value_counts()
+    plt.figure(figsize=(5,6))
+    plt.bar(count.index,count.values)
+    plt.title("Count of Placement Status")
+    plt.xlabel("Placement Status")
+    plt.ylabel("Count")
+    plt.savefig(os.path.join(BASE_DIR, "Placement_Status.png"))
+    plt.show()
+
+def univariate_eda(df):
+    plt.figure(figsize=(5,6))
+    plt.hist(df["CGPA"], bins=10)
+    plt.title("Histogram of CGPA")
+    plt.xlabel("CGPA")
+    plt.ylabel("Frequency")
+    plt.savefig(os.path.join(BASE_DIR, "CGPA_Histogram.png"))
+    plt.show()
+    gendercount = df["Gender"].value_counts()
+    plt.figure(figsize=(6, 5))
+    plt.pie(gendercount, labels=gendercount.index, autopct="%1.1f%%", startangle=90)
+    plt.title("Distribution of Gender")
+    plt.xlabel("Gender")
+    plt.savefig(os.path.join(BASE_DIR, "Gender_Count.png"))
+    plt.show()
+
+def bivariate(df):
+    plt.figure(figsize = (6,5))
+    plt.scatter(df["CGPA"], df["AptitudeTestScore"])
+    plt.title("CGPA vs Aptitude Test Score")
+    plt.xlabel("CGPA")
+    plt.ylabel("Aptitude Test Score")
+    plt.savefig(os.path.join(BASE_DIR, "CGPA_vs_Aptitude_Test_Score.png"))
+    plt.show()
+    plt.close()
+
+    placed = df[df["PlacementStatus"] == 1]["CGPA"]
+    not_placed = df[df["PlacementStatus"] == 0]["CGPA"]
+    plt.boxplot([placed, not_placed], label=["placed", "not_placed"])
+    plt.title("CGPA vs PlacementStatus")
+    plt.xlabel("PlacementStatus")
+    plt.ylabel("CGPA")
+    plt.savefig(os.path.join(BASE_DIR, "CGPA_vs_PlacementStatus.png"))
+    plt.show()
+
+
+def multivariate(df):
+    data = df[["CGPA", "AptitudeTestScore", "PlacementStatus"]]
+    correlation=data.corr()
+    plt.figure(figsize = (8,6))
+    sns.heatmap(correlation,annot=True,cmap="coolwarm",fmt=".2f")
+    plt.title("correlation heatmap")
+    plt.savefig(os.path.join(BASE_DIR, "correlation_heatmap.png"))
+    plt.show()
+    plt.close()
+
+if __name__ == "__main__":
+    df = load_data()
+    basic_eda(df)
+    univariate_eda(df)
+    bivariate(df)
+    multivariate(df)
